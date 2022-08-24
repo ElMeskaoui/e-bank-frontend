9c69fb99cd9aa7006f7eb523ad144077733e8d73
elmeskaoui <elhoucine.elmeskaoui@gmail.com>
2022-08-24 18:56:34 +0100

    2ed commit

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {error} from "@angular/compiler-cli/src/transformers/util";
+import {CustomerService} from "../services/customer.service";
 
 @Component({
   selector: 'app-customers',
@@ -9,10 +10,17 @@ import {error} from "@angular/compiler-cli/src/transformers/util";
 })
 export class CustomersComponent implements OnInit {
   customers: any;
-  constructor(private http:HttpClient) { }
+  constructor(private customerService:CustomerService) { }
 
   ngOnInit(): void {
-
+    this.customerService.getCustomers().subscribe({
+      next: (data)=>{
+        this.customers=data;
+      },
+      error: (error)=>{
+        console.log(error);
+      }
+    })
   }
 
 }
--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -7,13 +8,7 @@ import {HttpClient} from "@angular/common/http";
 export class CustomerService {
   customers: any;
   constructor(private http:HttpClient) { }
-  public getCustomers(){
-    this.http.get("http://localhost:8080/customers").subscribe({
-      next: (data)=>{
-        this.customers=data;
-      },
-      error: (error)=>{
-        console.log(error);
-      }
-    })  }
+  public getCustomers():Observable<any>{
+    return this.http.get("http://localhost:8080/customers")
+  }
 }
