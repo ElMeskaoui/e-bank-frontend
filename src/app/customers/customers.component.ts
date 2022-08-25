c22583220a00b0d7c5ca213e2080ffd834db4ac2
elmeskaoui <elhoucine.elmeskaoui@gmail.com>
2022-08-25 14:36:28 +0100

    4ed commit

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {error} from "@angular/compiler-cli/src/transformers/util";
 import {CustomerService} from "../services/customer.service";
-import {catchError, Observable, throwError} from "rxjs";
+import {catchError, map, Observable, throwError} from "rxjs";
 import {Customer} from "../models/customer.model";
 import {FormBuilder, FormGroup} from "@angular/forms";
 
@@ -37,5 +37,26 @@ export class CustomersComponent implements OnInit {
         return throwError(err);
       })
     );
+  }
+
+  handleDeleteCustomer(c: Customer) {
+    let conf = confirm("Are you sure?")
+    if (!conf) return;
+    this.customerService.deleteCustomer(c.id).subscribe(
+      {
+        next : (resp)=>{
+          this.customers=this.customers.pipe(
+            map(data=>{
+              let index=data.indexOf(c);
+              data.slice(index,1);
+              return data;
+            })
+          )
+        },
+        error : err => {
+          console.log(err);
+        }
+      }
+    )
   }
 }
